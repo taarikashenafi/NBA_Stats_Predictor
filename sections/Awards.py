00f388fc0cb286e3998d7482dfa0bf32872df06a
Taarik Ashenafi <taarik@Taariks-MacBook-Pro.local>
2024-09-27 22:53:15 -0500

    added assets folder

--- sections/Awards.py
+++ sections/Awards.py
@@ -3,4 +3,4 @@ def awards():
     st.title("Awards Predictions")
     st.header("🚧 Coming soon!")
     st.write("This page will display predictions for NBA Awards.")
-    st.image(".assets/construction.png")
+    st.image("./assets/construction.png")
